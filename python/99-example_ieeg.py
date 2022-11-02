39860c8c718282f942779a6ba7499a13137c7eaa
haoershi <116624350+haoershi@users.noreply.github.com>
2022-11-01 22:18:13 -0400

    Update 99-example_ieeg.py

--- python/99-example_ieeg.py
+++ python/99-example_ieeg.py
@@ -14,7 +14,7 @@ import os
 
 
 # %%
-with open(os.path.realpath("../config.json"), 'rb') as f:
+with open(os.path.realpath("./config.json"), 'rb') as f:
     config = pd.read_json(f, typ='series')
 
 iEEG_filename = "HUP172_phaseII"
