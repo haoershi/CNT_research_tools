5688da8c7b823b64d48e95735691f91ee3fbc45c
haoershi <116624350+haoershi@users.noreply.github.com>
2022-11-12 00:33:11 -0500

    Update test_func.m

--- matlab/test/test_func.m
+++ matlab/test/test_func.m
@@ -5,6 +5,8 @@ end
 
 % define specific test functions below
 function test_channel(testCase)
+work_path = pwd;
+addpath(genpath('./..'))
 actSolution = decompose_labels({'LA01','C3', 'LOC'},'HUP022');% can also read from file
 expSolution = {'LA1','C3', 'LOC'}';
 verifyEqual(testCase,actSolution,expSolution)
