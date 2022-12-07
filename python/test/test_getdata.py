8d00693cf1e61a06af50b379021a4c2e83fe7db8
haoershi <116624350+haoershi@users.noreply.github.com>
2022-12-07 14:41:33 -0500

    Add files via upload

--- python/test/test_getdata.py
+++ python/test/test_getdata.py
@@ -30,6 +30,8 @@ with open(os.path.join("python/config.json"), 'rb') as f:
         config = pd.read_json(f, typ='series')
 
 test_input = pd.read_csv(os.path.join(test_path,'getData_testInput.csv'))
+test_input['start'].astype(int)
+test_input['stop'].astype(int)
 params = [tuple(test_input.iloc[i,0:4]) for i in range(test_input.shape[0])]
 @pytest.mark.parametrize('filename,start,stop,out',params)
 def test_getdata(filename,start,stop,out):
