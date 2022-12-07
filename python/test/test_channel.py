b200481652ccdb83bfafcfe52826ecc94bb175b5
haoershi <116624350+haoershi@users.noreply.github.com>
2022-12-07 14:49:07 -0500

    Add files via upload

--- python/test/test_channel.py
+++ python/test/test_channel.py
@@ -25,7 +25,7 @@ import tools
 # unit test for clean_labels function
 # write in a csv file, col 0 input, col 1 expected output
 # wait to fetch all channel types from ieeg
-test_chans = pd.read_csv(os.path.join(test_path,'decomposeLabel_testInput.csv'))
+test_chans = pd.read_csv(os.path.join(test_path,'decompLabel_testInput.csv'))
 params = [tuple([[test_chans.iloc[i,0]],[test_chans.iloc[i,1]]]) for i in range(test_chans.shape[0])]
 @pytest.mark.parametrize('input,expected',params)
 def test_channel(input,expected):
