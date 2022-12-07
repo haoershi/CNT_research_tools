e517c2cb5db9d01c37bbff80a596011121e7b171
haoershi <116624350+haoershi@users.noreply.github.com>
2022-12-07 12:39:42 -0500

    Add files via upload

--- python/test/test_getdata.py
+++ python/test/test_getdata.py
@@ -29,16 +29,18 @@ import tools
 with open(os.path.join("python/config.json"), 'rb') as f:
         config = pd.read_json(f, typ='series')
 
-test_input = pd.read_csv(os.path.join(test_path,'testinput.csv'))
+meta_data = pd.read_csv(os.path.join(test_path,'metadata.csv'))
+test_input = pd.read_csv(os.path.join(test_path,'test_getData.csv'))
 params = [tuple(test_input.iloc[i,0:3]) for i in range(test_input.shape[0])]
-@pytest.mark.parametrize('filename,start,end',params)
+@pytest.mark.parametrize('filename,start,stop',params)
 def test_getdata(filename,start,end):
     try:
         data,fs = tools.get_iEEG_data(config.usr,config.pwd,filename,start,end)
         assert data.shape[0] == fs*(end-start)
     except Exception as e:
-        if type(e) == NameError or type(e) == ValueError:
+        if type(e)== NameError:
+            assert True
+        elif type(e)== ValueError:
             assert True
         else:
             assert False
-# %%
