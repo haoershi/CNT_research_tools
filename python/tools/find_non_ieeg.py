5732b0ad29244eff4727a8b9cd1952487d43156f
haoershi <116624350+haoershi@users.noreply.github.com>
2022-12-12 13:16:36 -0500

    Add files via upload

--- python/tools/find_non_ieeg.py
+++ python/tools/find_non_ieeg.py
@@ -3,6 +3,7 @@
 '''
 import re
 import numpy as np
+from beartype import beartype
 
 non_ieeg = [
     "EKG",
@@ -11,19 +12,24 @@ non_ieeg = [
     "ECG"
     ]
 
-def find_non_ieeg(channel_li):
+@beartype
+def find_non_ieeg(channel_li:Union[list[str],str]):
     '''
     This function finds non-iEEG channel labels
     '''
-
+    if not isinstance(channel_li,list):
+        channel_li = [channel_li]
     is_non_ieeg = np.zeros(len(channel_li), dtype=bool)
     for ind, i in enumerate(channel_li):
         # Gets letter part of channel
-        regex_match = re.match(r"(\D+)(\d+)", i)
-        lead = regex_match.group(1)
-
+        label_num_search = re.search(r'\d', i)
+        if label_num_search is not None:
+            label_num_idx = label_num_search.start()
+            label_non_num = channel_li[i][:label_num_idx]
+        else:
+            label_non_num = i
         # finds non-iEEG channels, make a separate function
-        if lead in non_ieeg:
+        if label_non_num in non_ieeg:
             is_non_ieeg[ind] = True
 
     return is_non_ieeg
