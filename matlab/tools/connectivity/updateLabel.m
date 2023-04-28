c5f6f4a3f825d29849400f39557d670859b59b83
haoershi <haoershi@seas.upenn.edu>
2023-04-28 16:35:27 -0400

    Delete matlab/tools/connectivity directory

--- matlab/tools/connectivity/updateLabel.m
+++
@@ -1,6 +0,0 @@
-function [values,newlabels,newInd,keep] = updateLabel(values,labels,include)
-load('elecmat.mat');
-newlabels = labels(include);
-[newInd,keep] = findLRelecs(newlabels);
-newlabels = [leftmat(logical(newInd));rightmat(logical(newInd))];
-values = values(:,ismember(labels,newlabels));
