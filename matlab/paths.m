7f85fbdb2a36bfcd305f403aef7d028669be8f08
haoershi <haoershi@seas.upenn.edu>
2023-11-02 02:27:22 -0400

    update

--- matlab/paths.m
+++ matlab/paths.m
@@ -1,10 +1,11 @@
 % Global settings of the CNT iEEG Pre-processing Toolkit
-global ROOT_DIR TEST_DIR DATA_DIR USER_DIR
+global ROOT_DIR TEST_DIR DATA_DIR USER_DIR TESTDATA_DIR
 % Directories
 ROOT_DIR = fileparts(mfilename('fullpath'));
 TEST_DIR = fullfile(ROOT_DIR, 'test');
 DATA_DIR = fullfile(ROOT_DIR, 'data');
 USER_DIR = fullfile(ROOT_DIR, 'users');
+TESTDATA_DIR = fullfile(TEST_DIR, 'data');
 
 DIRS = {DATA_DIR, USER_DIR};
 
